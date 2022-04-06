f48d3b0c8dd57fd33e8590b41f80d8dc0f47e91d
Alberto Rosa <alberto.r.93@hotmail.it>
2022-04-06 16:35:55 +0200

    setup

--- csharp-classe-generation/Program.cs
+++ csharp-classe-generation/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+nomi
+cognomi
+eta
+numero di partecipanti attuali nel corso
+
+Testate le vostre funzioni di statistiche, per esempio aggiungendo l'opzione "statistiche" per l'utente, che una volta scritto a console "statistiche" vi stampi i risultati delle funzioni appena dichiarate.
+*/
+
+
+string[] nomiAlunni = new string[10]; //array di stringhe dimensione 10
+string[] cognomiAlunni = new string[10];
+string[] etàAlunni = new string[10];
+int partecipantiNow = 0;
+//proviamo: voglio salvare l'enumeratore più vecchio, giovane e con più lettere ad ogni aggiunta, per recuperarli poi
+int theOldestPartecipante = 0;
+int theYoungestPartecipante = 0;
+
+// --------------------------FUNZIONI--------------------------
+
+//Stampacognomi
+
+//stampanome
+
+//stampaetà
+
+//aggiungiAlunno
+
+//rimuoviAlunno
+
+//-----------------------FUNZIONI AVANZATE----------------------
+
+
+//calcola età media classe
+
+//the Oldest
+
+//the Youngest
+
+
+
+//-------------------------------------------------------------
+//---------------------L'ESEGUIBILE----------------------------
+//-------------------------------------------------------------
